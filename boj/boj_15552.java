6974d2ab442233c7827cb63b35b286a49a4b9612
PANGHWAN2 <raccoonste@naver.com>
2024-06-03 17:41:55 +0900

    boj_15552

--- boj/boj_15552.java
+++ boj/boj_15552.java
@@ -1,24 +1,22 @@
-package boj;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-// boj_15552.java
-import java.io.*;
 
+//백준_15552 빠른A+B
 public class boj_15552 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
-        
-        for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split(" ");
-            int a = Integer.parseInt(input[0]);
-            int b = Integer.parseInt(input[1]);
-            bw.write((a + b) + "\n");
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
         }
-        
-        bw.flush();
         bw.close();
-        br.close();
     }
 }
