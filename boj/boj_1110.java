39032e7bd0b0f0ef39528c150389811206df1973
PANGHWAN2 <raccoonste@naver.com>
2024-06-12 22:08:18 +0900

    update

--- boj/boj_1110.java
+++ boj/boj_1110.java
@@ -6,7 +6,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+// 1110
 public class boj_1110 {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
