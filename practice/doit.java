7dbf4fe7ae920fbf2bd219e117c7cd2b97dd6f83
panghwan1031 <panghwan1031@gmail.com>
2025-03-12 23:43:26 +0900

    0312

--- practice/doit.java
+++ practice/doit.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class doit {
     // 숫자의 합 구하기
@@ -119,5 +120,100 @@ public class doit {
     //재귀함수
     //스택 자료구조 이용
 
+    //연결 요소의 개수 구하기
+
+    // static boolean v[];
+    // static ArrayList<Integer>[] A;
+    // public static void main(String[] args)throws IOException{
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     StringTokenizer st = new StringTokenizer(br.readLine());
+    //     int n = Integer.parseInt(st.nextToken());
+    //     int m = Integer.parseInt(st.nextToken());
+
+    //     v = new boolean[n + 1];
+    //     A = new ArrayList[n + 1];
+    //     for(int i = 1; i < n + 1; i++){
+    //         A[i] = new ArrayList<Integer>();
+    //     }
+    //     for(int i = 0 ; i < m; i++){
+    //         st = new StringTokenizer(br.readLine());
+    //         int s = Integer.parseInt(st.nextToken());
+    //         int e = Integer.parseInt(st.nextToken());
+    //         A[s].add(e);
+    //         A[e].add(s);
+    //     }
+    //     int cnt = 0;
+    //     for(int i = 1; i < n + 1; i++){
+    //         if(!v[i]){
+    //             cnt++;
+    //             dfs(i);
+    //         }
+    //     }
+    //     System.out.println(cnt);
+    // }
+    // private static void dfs(int i){
+    //     if(v[i]){
+    //         return;
+    //     }
+    //     v[i] = true;
+    //     for(int j : A[i]){
+    //         if(!v[j]){
+    //             dfs(j);
+    //         }
+    //     }
+    // }
+
+    //DFS 우선순회
+    private static ArrayList<Integer>[] adjList;
     
+    private static boolean[] visited;
+    private static ArrayList<Integer> answer;
+
+    private static int[] solution(int[][] graph, int start, int n){
+        adjList = new ArrayList[n + 1];
+        for(int i = 0; i < adjList.length; i++){
+            adjList[i] = new ArrayList<>();
+        }
+        for(int[] edge : graph){
+            adjList[edge[0]].add(edge[1]);
+            //adjList[edge[1]].add(edge[0]);
+        }
+        visited = new boolean[ n + 1 ];
+        answer = new ArrayList<>();
+        dfs(start);
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static void dfs(int now){
+        visited[now] = true;
+        answer.add(now);
+
+        for(int next : adjList[now]){
+            if(!visited[next])dfs(next);
+        }
+    }
+
+    public static void main(String[] args){
+        int[][] graph = {
+            {1, 2},
+            {1, 3},
+            {2, 4},
+            {2, 5},
+            {3, 6},
+            {5, 6},
+            // {4, 8},
+            // {5, 9},
+            // {5, 10},
+            // {6, 11},
+            // {6, 12},
+            // {9, 13},
+            // {10, 14},
+            // {12, 15}
+        };
+        int[] result = solution(graph, 1, 6);
+        for(int i : result){
+            System.out.print(i + " ");
+    }
+    }
 }
