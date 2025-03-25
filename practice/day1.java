ad2180edfa7dc67bf512bb22f75da7f982239c6b
panghwan1031 <panghwan1031@gmail.com>
2025-03-25 23:55:14 +0900

    day2

--- practice/day1.java
+++ practice/day1.java
@@ -8,18 +8,19 @@ import java.util.Arrays;
 
 public class day1 {
 
+
     //1037
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String[] str = br.readLine().split(" ");
-        int[] arr = new int[n];
-        for(int i = 0 ; i < n; i++){
-            arr[i] = Integer.parseInt(str[i]);
-        }
-        Arrays.sort(arr);
-        System.out.println(arr[0] * arr[n - 1]);
-    }
+    // public static void main(String[] args)throws IOException{
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     int n = Integer.parseInt(br.readLine());
+    //     String[] str = br.readLine().split(" ");
+    //     int[] arr = new int[n];
+    //     for(int i = 0 ; i < n; i++){
+    //         arr[i] = Integer.parseInt(str[i]);
+    //     }
+    //     Arrays.sort(arr);
+    //     System.out.println(arr[0] * arr[n - 1]);
+    // }
 
     //1037
     // public static void main(String[] args)throws IOException{
